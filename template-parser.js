bc023dcd57b327f207d7a411b64a64b0197c7051
mayormaier <maierandrewd@gmail.com>
2022-10-22 12:44:42 -0400

    add method to write yaml template

--- template-parser.js
+++ template-parser.js
@@ -1,29 +1,31 @@
 import * as fs from "node:fs"
+import { Buffer } from "node:buffer"
 import Ajv from "ajv"
 import fetch from "node-fetch"
 import { parse, stringify, YAMLError } from "yaml"
+import { writeFile } from "node:fs"
 
 const schemaLocation = 'https://raw.githubusercontent.com/elmsln/kraxen/main/keycloak-deploy.schema.json'
 
 export async function validateTemplate(template) {
-    // use ajv https://ajv.js.org/
-
     let res = await fetch(schemaLocation)
     const schema = await res.json()
 
     const validator = new Ajv({strict: "log"})
     const validate = validator.compile(schema)
     return validate(template)
-
 }
 
 export function parseTemplate(templatePath) {
-    // use yaml https://www.npmjs.com/package/yaml
-
     let template = fs.readFileSync(templatePath, 'utf8')
     return parse(template)
 }
 
-export function buildTemplate() {
-    // build a yaml template based on CLI, save to filesystem.
+export function buildTemplate(values) {
+    // builds a yaml template based on CLI, save to filesystem.
+    const template = stringify(values)  
+    const data = new Uint8Array(Buffer.from(template))
+    writeFile('keycloak-client.template.yaml', data, (err) => {
+        if (err) throw err
+    })
 }
