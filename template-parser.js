165a41508b7026cee7a460fec2cdaae98085e49c
mayormaier <maierandrewd@gmail.com>
2022-11-01 12:09:33 -0400

    read template locally/synchronously

--- template-parser.js
+++ template-parser.js
@@ -1,36 +1,29 @@
-const { fs } = require("node:fs")
-const { Buffer } = require("node:buffer")
-const { Ajv } = require("ajv")
-const { fetch } = import("node-fetch")
-const { parse, stringify, YAMLError } = require("yaml")
+import {writeFile, readFileSync} from "fs"
+import { Buffer } from "node:buffer"
+import Ajv from "ajv"
+import { parse, stringify} from "yaml"
 
-const schemaLocation = 'https://raw.githubusercontent.com/elmsln/kraxen/main/keycloak-deploy.schema.json'
+const schemaPath = './keycloak-deploy.schema.json'
 
-const validateTemplate = async function validateTemplate(template) {
-    let res = await fetch(schemaLocation)
-    const schema = await res.json()
+export function validateTemplate(template) {
+    let res = readFileSync(schemaPath, 'utf-8')
+    const schema = JSON.parse(res)
 
     const validator = new Ajv({strict: "log"})
     const validate = validator.compile(schema)
     return validate(template)
 }
 
-const parseTemplate = function parseTemplate(templatePath) {
-    let template = fs.readFileSync(templatePath, 'utf8')
+export function parseTemplate(templatePath) {
+    let template = readFileSync(templatePath, 'utf8')
     return parse(template)
 }
 
-const buildTemplate = function buildTemplate(values) {
+export function buildTemplate(values) {
     // builds a yaml template based on CLI, save to filesystem.
     const template = stringify(values)  
     const data = new Uint8Array(Buffer.from(template))
-    fs.writeFile('keycloak-client.template.yaml', data, (err) => {
+    writeFile('keycloak-client.template.yaml', data, (err) => {
         if (err) throw err
     })
-}
-
-module.exports = {
-    validateTemplate,
-    parseTemplate,
-    buildTemplate
 }
